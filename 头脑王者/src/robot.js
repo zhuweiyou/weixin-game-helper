ff2152204066e5f8914c322863d4ec190184a17b
zhuweiyou <i@zhuweiyou.com>
2018-01-17 16:42:58 +0800

    更新 robot

--- 头脑王者/src/robot.js
+++ 头脑王者/src/robot.js
@@ -5,24 +5,31 @@ const QuizModel = require('./database/quiz-model')
 module.exports = {
   async beforeSendRequest (requestDetail) {
     if (requestDetail.url.indexOf('/choose') !== -1) {
-      // 提交答案时，修改答案，重新计算签名后发送
+      // 提交答案时，从题库中匹配答案，重新计算签名后发送
       const data = QueryString.parse(requestDetail.requestData)
-      data.sign = Tnwz.sign(data, this._login.token)
-      requestDetail.requestData = QueryString.stringify(data)
-      return requestDetail
+      const one = await QuizModel.findOne({quiz: this._findQuiz.quiz})
+      if (one) {
+        this._quiz = one
+        // 由于选项的顺序每一次可能不一样，所以从这次选项中，找题库里正确的那个文字
+        one.options = this._findQuiz.quiz.options.findIndex(option => option === one.options[one.answer - 1])
+        data.sign = Tnwz.sign(data, this._login.token)
+        requestDetail.requestData = QueryString.stringify(data)
+        return requestDetail
+      } else {
+        this._quiz = null
+      }
     }
   },
   async beforeSendResponse (requestDetail, responseDetail) {
-    const response = responseDetail.response
+    const response = responseDetail.response.body.data
     if (requestDetail.url.indexOf('/login') !== -1) {
       this._login = response
     } else if (requestDetail.url.indexOf('/findQuiz') !== -1) {
       this._findQuiz = response
     } else if (requestDetail.url.indexOf('/choose') !== -1) {
       // 提交完答案，会返回正确答案，如果题库没有，就存起来
-      const one = await QuizModel.findOne({quiz: this._findQuiz.data.quiz})
-      if (!one) {
-        const quizModel = new QuizModel(Object.assign(this._findQuiz.data, {answer: response.answer}))
+      if (!this._quiz) {
+        const quizModel = new QuizModel(Object.assign(this._findQuiz, {answer: response.answer}))
         await quizModel.save()
       }
     }
