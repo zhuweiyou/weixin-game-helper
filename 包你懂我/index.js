d7f99b1fb0f517890f93a52944d3edca0cfb7b04
zhuweiyou <i@zhuweiyou.com>
2018-01-23 10:34:45 +0800

    优化代码

--- 包你懂我/index.js
+++ 包你懂我/index.js
@@ -1,25 +1,21 @@
 module.exports = {
-  * beforeSendRequest (requestDetail) {
-  },
+  * beforeSendRequest (requestDetail) {},
   * beforeSendResponse (requestDetail, responseDetail) {
     let data
     let body
     try {
       body = JSON.parse(responseDetail.response.body.toString())
       data = body.data
-    } catch (e) {
-
-    }
+    } catch (e) {}
     if (requestDetail.url.indexOf('/minapp/answer/question/') !== -1) {
-      this._findQuiz = data
       if (data.question) {
         //匹配出正确答案
-        data.question.forEach(function (value, index) {
-          answer = value.answers[value.right]
+        data.question.forEach((value, index) => {
+          const answer = value.answers[value.right]
           console.log(value.title + '答案:' + answer)
-          data.question[index].answers[value.right] = '√' + answer
+          value.answers[value.right] = '√ ' + answer
         })
-        body.data = this._findQuiz
+        body.data = data
         const response = Object.assign({}, responseDetail.response)
         response.body = JSON.stringify(body)
         return {response}
