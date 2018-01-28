8ee0e34d30efcc2e07995bf5e996ca74d0923957
zhuweiyou <i@zhuweiyou.com>
2018-01-28 19:03:47 +0800

    完成

--- 大家来找茬腾讯版/proxy/README.md
+++ 大家来找茬腾讯版/proxy/README.md
@@ -1,7 +1,20 @@
 # 大家来找茬腾讯版
 
-正在开发中
+自动用红色区块，标记所有不同之处
 
+## 准备
 
+- Node.js 9.x 以上
+- 安装依赖 `npm i`
 
+## 使用
 
+执行 `npm run ca` 生成 RootCA，将生成的证书安装到手机上，并在手机上设置代理 IP（电脑的局域网 IP） 和 端口（8001）
+
+证书配置遇到困难可以看[这里](http://anyproxy.io/cn/#%E8%AF%81%E4%B9%A6%E9%85%8D%E7%BD%AE)
+
+一切都配置好之后，**完全退出游戏进程**，执行 `npm start`，再重新进入游戏
+
+## 截图
+
+<img src="https://user-images.githubusercontent.com/8413791/35481371-571a8a86-045d-11e8-8130-f235049dfcb1.png" alt="截图">
--- 大家来找茬腾讯版/proxy/src/index.js
+++ 大家来找茬腾讯版/proxy/src/index.js
@@ -4,7 +4,7 @@ module.exports = {
   * beforeSendRequest (requestDetail) {},
   * beforeSendResponse (requestDetail, responseDetail) {
     if (requestDetail.url.indexOf('/mixed_image.dat') !== -1) {
-      const id = url.match(/\/([^/]*?)\/mixed_image/).pop()
+      const id = requestDetail.url.match(/\/([^/]*?)\/mixed_image/).pop()
       const response = responseDetail.response
       response.body = decrypt(id, response.body)
       return {response}
