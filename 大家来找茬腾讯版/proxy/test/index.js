b73065f5cd2fba0de056fa3f0bd310d042eee8d6
zhuweiyou <i@zhuweiyou.com>
2018-01-28 18:36:24 +0800

    开发中

--- 大家来找茬腾讯版/proxy/test/index.js
+++ 大家来找茬腾讯版/proxy/test/index.js
@@ -2,6 +2,6 @@ const fs = require('fs-extra')
 const decrypt = require('../src/decrypt')
 
 // 运行 npm test
-// .dat 为游戏请求的资源，解密后写出 png 看下效果
+// .dat 是游戏加密的资源，解密后写出到 data/14087.png 看效果
 ;(async () => decrypt(14087, await fs.readFile('./test/14087.dat')))()
 
