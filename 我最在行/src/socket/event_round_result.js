aac02400c7755c4c0ff7674931c644e763c9bc1c
zhuweiyou <i@zhuweiyou.com>
2018-02-08 14:12:04 +0800

    修复

--- 我最在行/src/socket/event_round_result.js
+++ 我最在行/src/socket/event_round_result.js
@@ -5,8 +5,8 @@ module.exports = (socket, data) => {
   if (socket.master) {
     // 组合题目和答案，入库
     socket._question.answer = data.results.answer
-    new QuestionModel(this._question).save()
-    console.log('保存到题库', JSON.stringify(this._question))
+    new QuestionModel(socket._question).save()
+    console.log('保存到题库', JSON.stringify(socket._question))
 
     // 下一题的题目，也一块返回了。和 event_battle_start 一样，暂存内存
     socket._question = {
