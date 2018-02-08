d85ed5d8fbad9267c5abf28de4f24fbc121018db
zhuweiyou <i@zhuweiyou.com>
2018-02-08 14:20:01 +0800

    完成爬题脚本

--- 我最在行/src/socket/event_round_result.js
+++ 我最在行/src/socket/event_round_result.js
@@ -5,7 +5,12 @@ module.exports = async (socket, data) => {
   if (socket.master) {
     // 组合题目和答案，入库
     socket._question.answer = data.results.answer
-    console.log('保存到题库', JSON.stringify(await new QuestionModel(socket._question).save()))
+
+    if (await QuestionModel.findOne({question: socket._question.question})) {
+      console.log('题库已有，不再入库')
+    } else {
+      console.log('保存到题库', JSON.stringify(await new QuestionModel(socket._question).save()))
+    }
 
     // 下一题的题目，也一块返回了。和 event_battle_start 一样，暂存内存
     socket._question = {
